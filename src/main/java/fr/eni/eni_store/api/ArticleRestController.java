30f834ed59e4655bfc7e2fb0c8e0f865b4c2df6c
Chocolaterie <110991127+Chocolaterie@users.noreply.github.com>
2025-08-20 09:57:41 +0200

    Correction TP 05

--- src/main/java/fr/eni/eni_store/api/ArticleRestController.java
+++ src/main/java/fr/eni/eni_store/api/ArticleRestController.java
@@ -3,6 +3,7 @@ package fr.eni.eni_store.api;
 import fr.eni.eni_store.bo.Article;
 import fr.eni.eni_store.service.ArticleService;
 import fr.eni.eni_store.service.ServiceResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,24 +16,29 @@ public class ArticleRestController {
     @Autowired
     ArticleService articleService;
 
+    @Operation(summary = "Endpoint pour récupérer tout les articles")
     @GetMapping("all")
     public ServiceResponse<List<Article>> getAll(){
 
         return articleService.getAll();
     }
 
+    @Operation(summary = "Endpoint pour récupérer un article")
     @GetMapping("get/{id}")
     public ServiceResponse<Article> getById(@PathVariable Integer id){
 
         return articleService.getById(id);
     }
 
+    @Operation(summary = "Endpoint pour supprimer un article")
     @DeleteMapping("{id}")
     public ServiceResponse<Article> deleteById(@PathVariable Integer id){
 
         return articleService.deleteById(id);
     }
 
+    @Operation(summary = "Endpoint pour enregistrer un article",
+            description = "Attention si l'article n'existe pas c'est une création. Sinon une mise à jour")
     @PostMapping("save")
     public ServiceResponse<Article> save(@RequestBody Article article){
 

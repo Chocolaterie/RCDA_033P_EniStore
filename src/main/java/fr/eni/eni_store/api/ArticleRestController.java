2f74f0e7cfdceef2610709586b2c9f2e0321727a
Chocolaterie <ischartier@campus-eni.fr>
2025-08-25 15:43:11 +0200

    Correction TP

--- src/main/java/fr/eni/eni_store/api/ArticleRestController.java
+++ src/main/java/fr/eni/eni_store/api/ArticleRestController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/articles")
 public class ArticleRestController {
--- src/main/java/fr/eni/eni_store/api/AuthRestController.java
+++ src/main/java/fr/eni/eni_store/api/AuthRestController.java
@@ -3,10 +3,12 @@ package fr.eni.eni_store.api;
 import fr.eni.eni_store.service.AuthService;
 import fr.eni.eni_store.service.ServiceResponse;
 import fr.eni.eni_store.service.dto.LoginRequest;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class AuthRestController {
 
